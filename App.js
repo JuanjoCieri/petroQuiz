dc8043cc50459fc77340466278178d6d48b79f79
JuanjoCieri <juin@hotmail.com.ar>
2023-08-16 16:59:39 -0300

    first

---
+++ App.js
@@ -0,0 +1,8 @@
+import AppNavigation from './src/navigation/appNavigation';
+
+
+export default function App() {
+  return (
+    <AppNavigation />
+  );
+}
---
+++ src/components/AppBar.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import {View, Text} from "react-native"
+
+export default function AppBar () {
+    return (
+        <View>
+            <Text>HOla</Text>
+        </View>
+    )
+}
