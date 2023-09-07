d2b4dcfc2ef5398b08f42f4a0b1d297fca416048
JuanjoCieri <juin@hotmail.com.ar>
2023-09-06 21:53:42 -0300

    google

--- App.js
+++ App.js
@@ -1,54 +1,8 @@
 import { Provider, useDispatch } from "react-redux";
 import AppNavigation from "./src/navigation/appNavigation";
 import { store } from "./src/redux/store";
-// import * as Google from "expo-auth-session/providers/google";
-// import {
-//   GoogleAuthProvider,
-//   onAuthStateChanged,
-//   signInWithCredential,
-// } from "firebase/auth";
-// import * as WebBrowser from "expo-web-browser";
-// import { auth } from "./firebase-config";
-// import { useEffect } from "react";
-// import LandingScreen from "./src/screens/LandingScreen/LandingScreen";
-// import { postAuthenticateWithGoogle } from "./src/redux/Actions";
-
-// WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
-  // const dispatch = useDispatch();
-  // const [request, response, promptAsync] = Google.useAuthRequest(
-  //   {
-  //     iosClientId:
-  //       "611019823270-2rahov9v0p3fqufrebtv5io6ekdegn2m.apps.googleusercontent.com",
-  //     androidClientId:
-  //       "611019823270-jvic2hkktr81eprhfrg4u08ti57d69ov.apps.googleusercontent.com",
-  //     webClientId:
-  //       "611019823270-h71ppt61t8q0re3868sb6tc7i95ajj0j.apps.googleusercontent.com",
-  //   },
-  //   { native: "blank://" }
-  // );
-  // useEffect(() => {
-  //   if (response?.type == "success") {
-  //     const { id_token } = response.params;
-  //     const credential = GoogleAuthProvider.credential(id_token);
-  //     signInWithCredential(auth, credential);
-  //   }
-  // }, [response]);
-  // useEffect(() => {
-  //   const unsub = onAuthStateChanged(auth, async (user) => {
-  //     if (user) {
-  //       const payload = {
-  //         uid: user.uid,
-  //         displayName: user.displayName,
-  //         email: user.email,
-  //         photoURL: user.photoURL,
-  //       };
-  //       dispatch(postAuthenticateWithGoogle(payload));
-  //     }
-  //   });
-  //   return () => unsub();
-  // }, []);
 
   return (
     <Provider store={store}>
--- src/screens/LandingScreen/LandingScreen.js
+++ src/screens/LandingScreen/LandingScreen.js
@@ -9,7 +9,21 @@ import {
 import { useDispatch } from "react-redux";
 import { postAuthenticateWithGoogle } from "../../redux/Actions";
 
-export default function LandingScreen({ promptAsync }) {
+/////////////////
+
+import * as Google from "expo-auth-session/providers/google";
+import {
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithCredential,
+} from "firebase/auth";
+import * as WebBrowser from "expo-web-browser";
+import { auth } from "../../../firebase-config";
+import { useEffect } from "react";
+
+WebBrowser.maybeCompleteAuthSession();
+
+export default function LandingScreen() {
   const dispatch = useDispatch();
 
   const aad = () => {
@@ -22,6 +36,45 @@ export default function LandingScreen({ promptAsync }) {
     };
     dispatch(postAuthenticateWithGoogle(payload));
   };
+
+///////////////////////////////////////
+
+// const dispatch = useDispatch();
+  const [request, response, promptAsync] = Google.useAuthRequest(
+    {
+      iosClientId:
+        "611019823270-2rahov9v0p3fqufrebtv5io6ekdegn2m.apps.googleusercontent.com",
+      androidClientId:
+        "611019823270-jvic2hkktr81eprhfrg4u08ti57d69ov.apps.googleusercontent.com",
+      webClientId:
+        "611019823270-h71ppt61t8q0re3868sb6tc7i95ajj0j.apps.googleusercontent.com",
+    },
+    { native: "blank://" }
+  );
+  useEffect(() => {
+    if (response?.type == "success") {
+      const { id_token } = response.params;
+      const credential = GoogleAuthProvider.credential(id_token);
+      signInWithCredential(auth, credential);
+    }
+  }, [response]);
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const payload = {
+          uid: user.uid,
+          displayName: user.displayName,
+          email: user.email,
+          photoURL: user.photoURL,
+        };
+        dispatch(postAuthenticateWithGoogle(payload));
+      }
+    });
+    return () => unsub();
+  }, []);
+
+//////////////////////////////////////
+
   return (
     <ImageBackground
       source={require("../../../assets/profileBackground.png")}
@@ -33,9 +86,12 @@ export default function LandingScreen({ promptAsync }) {
           <Text>Bienvenidos a</Text>
           <Text style={{fontSize: 65}}>PetroQuiz</Text>
         </View>
-        <View>
+        <View style={{gap: 40}}>
           <TouchableOpacity onPress={aad}>
             <Text>Registrarse</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={promptAsync}>
+            <Text>Registrarse Google</Text>
           </TouchableOpacity>
         </View>
       </View>
